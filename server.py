a76ecac294eb5cd4c9ca496a5cd1d5bf53a79d77
Jonathan García <yugidr0@gmail.com>
2026-04-30 21:34:20 -0600

    refactor server to handle client connections in separate threads

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 def start_server():
   host = "127.0.0.1"
@@ -6,19 +7,29 @@ def start_server():
 
   sv_socket = socket.socket()
   sv_socket.bind((host, port)) 
-  sv_socket.listen(1)
+  sv_socket.listen()
 
-  connection, address = sv_socket.accept()
-  print("Connection from: " + str(address))
   while True: 
-    data = connection.recv(1024).decode()
-    if not data:
-      break
-    print("From connected user: " + str(data))
-    data = input(' > ')
-    connection.send(data.encode())
+    connection, address = sv_socket.accept()
+    print("Connection from: " + str(address))
 
-  connection.close()
+    thread = threading.Thread(target=handle_client, args=(connection, address))
+    thread.start()
+    print("Active connections: " + str(threading.active_count() - 1))
+   
   
+def handle_client(connection, address):
+  while True:
+    try:
+      data = connection.recv(1024).decode()
+      if not data:
+        break
+      print("Received from " + str(address) + ": " + data)
+      connection.send((data).encode())
+    except:
+      print("Connection with " + str(address) + " closed.")
+      break
+  connection.close()
+
 if __name__ == '__main__':
   start_server()
