9d662b7b1edb7061fe438a705b7c92cd240e181a
Jonathan García <yugidr0@gmail.com>
2026-05-02 18:34:15 -0600

    refactor server to replace print statements with logging for improved error handling and traceability

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 import socket
 import threading
+import logging
 
 def start_server():
   host = "127.0.0.1"
@@ -11,6 +12,12 @@ def start_server():
   sv_socket.bind((host, port)) 
   sv_socket.listen()
 
+  logging.basicConfig(
+    filename='server.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+  )
+
   blocked_ips = ["192.168.1.100"]  # Example of blocked IPs
 
   try:
@@ -18,17 +25,18 @@ def start_server():
       connection, address = sv_socket.accept()
 
       if address[0] in blocked_ips:
-        print(f"Blocked connection attempt from: {address}")
+        logging.info(f"Blocked connection attempt from: {address}")
         connection.close()
         continue
 
-      print(f"Connection from: {address}")
+      logging.info(f"Connection from: {address}")
 
       thread = threading.Thread(target=handle_client, args=(connection, address))
+      thread.daemon = True
       thread.start()
-      print(f"Active connections: {threading.active_count() - 1}")
+      logging.info(f"Active connections: {threading.active_count() - 1}")
   except KeyboardInterrupt:
-    print("\nServer is shutting down.")
+    logging.info("\nServer is shutting down.")
     sv_socket.close()
 
 def handle_client(connection, address):
@@ -38,14 +46,15 @@ def handle_client(connection, address):
       if not data:
         break
       package_received = json.loads(data)
+      logging.info(f"{package_received['user']}: {package_received['content']}")
       print(f"{package_received['user']}: {package_received['content']}")
       package_response = {
         "content": f"Server received your message: {package_received['content']}"
       }
       json_response = json.dumps(package_response)
       connection.sendall(json_response.encode())
-    except:
-      print(f"Connection with {address} closed.")
+    except Exception as e:
+      logging.error(f"Connection with {address} closed due to error: {e}")
       break
   connection.close()
 
